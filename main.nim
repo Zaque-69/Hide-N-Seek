c28e5f3171dc48624147eda2e6937ae772732614
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-01-13 01:15:09 +0200

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,20 +1,33 @@
-import std/strformat, osproc, os
+
+import std/strformat, osproc, os, nimpy
 
 let clearCmd : int = execCmd(fmt"clear")
 echo clearCmd
+
+echo """▒█░▒█ ░▀░ █▀▀▄ █▀▀   █ ▒█▄░▒█   ▒█▀▀▀█ █▀▀ █▀▀ █░█ 
+        ▒█▀▀█ ▀█▀ █░░█ █▀▀   ░ ▒█▒█▒█   ░▀▀▀▄▄ █▀▀ █▀▀ █▀▄ 
+        ▒█░▒█ ▀▀▀ ▀▀▀░ ▀▀▀   ░ ▒█░░▀█   ▒█▄▄▄█ ▀▀▀ ▀▀▀ ▀░▀
+     """
+
+proc pass() = return
+
+proc showCurrentDirrectoryFiles(path : string) : void =
+    pass()
 
 proc executeYaraRule(file, path : string) : void =
     let yaraRuleFile : bool = fileExists(file)
     let ansBool : int = execCmd(fmt"yara {file} {path}")
 
+
     if ansBool == 1 : 
         if yaraRuleFile == false : 
-            echo fmt"File {file} doesn't exist."
+            echo fmt"File '{file}' doesn't exist."
             return 
         else : 
-            echo fmt"Path {path} doesn't exist."
+            echo fmt"Path {path}' doesn't exist."
             return 
 
-    echo ansBool;
+    #echo ansBool;
 
-executeYaraRule("main.yara", "/home/z4que/Downloads")
+#executeYaraRule("main.yara", "/home/z4que/Downloads")
+showCurrentDirrectoryFiles("")
