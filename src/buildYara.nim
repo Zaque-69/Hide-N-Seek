065b88f836367f053d39c50e3facec2c0c864931
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-01-16 00:04:31 +0200

    Update buildYara.nim

--- src/buildYara.nim
+++ src/buildYara.nim
@@ -1,1 +1,22 @@
- 
+import json
+
+proc readFile(filename: string): string =
+  var
+    file: File
+    content: string
+  content = readAll(file)
+  close(file)
+  return content
+
+proc pass() = return
+
+proc main( fileExtension : string) =
+  let fileContent = readFile("extensions.json")
+
+  if fileContent.len > 0:
+    try:
+      let jsonData = parseJson(fileContent)
+
+      let extension = jsonData[fileExtension].getStr()
+
+    except : pass()
