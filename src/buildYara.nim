5dd4495c1138119f51d4bd6180b1f668cd2f035c
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-01-17 22:44:47 +0200

    Update buildYara.nim

--- src/buildYara.nim
+++ src/buildYara.nim
@@ -2,8 +2,12 @@ import json, times, strutils, std/strformat
 
 let init : float =  cpuTime()
 
-
 proc pass() = return
+var
+  yaraContent : string 
+  yaraStructure : string
+  list : array[10, string]
+
 
 proc writeYara(filename: string, content: string) =
   var 
@@ -24,31 +28,39 @@ proc readFileContent(filename: string): string =
   return content
 
 proc main( extensionFile : string) : void =
-
-  var
-    yaraStructure : string
-
+    
   let fileContent = readFileContent("extensions.json")
   let jsonData = parseJson(fileContent)
   let hexDecimals = jsonData[extensionFile].getStr()
 
-  if len(hexDecimals) > 0 : echo hexDecimals
+  if len(hexDecimals) > 0 : list[0] = hexDecimals
 
   else : 
-    for i in countup(1, 9) :
+    for i in countup(0, 9) :
       try :
         let secondExtensionFile = extensionFile & intToStr(i)
-        #                       byte(nr)      {     previsious text from file     endline     hex decimals from json (all from an extension)      }
-        yaraStructure = "\n" & "byte" & intToStr(i) & " = {" & readFileContent("newww.txt") & jsonData[extensionFile][secondExtensionFile].getStr() & "}"
-
-        #writeYara("newww.txt", yaraStructure)
-
-        echo yaraStructure
+        yaraStructure = readFileContent("newww.txt") & jsonData[extensionFile][secondExtensionFile].getStr()
+        list[i] = yaraStructure
         yaraStructure = ""
-
+        
 
       except : pass()
-      
-main("pem")
+
+main("jpg")
+
+proc buildYaraStructure(bytes : string, number : int) : void =
+  yaraContent = "rule" & intToStr(number) & " = { \n strings : \n"
+  for i in countup(0, 9): 
+    if len(list[i]) > 0 : yaraContent &= "$byte" & intToStr(i) & " = {" & list[i] & "} \n"
+  yaraContent &= "\n conditions : "
+  for i in countup(0, 9): 
+    if len(list[i]) > 0 : yaraContent &= "$byte" & intToStr(i) & " & "
+
+  echo yaraContent
+
+#
+for i in countup(0, 9 mod 2):
+  if len(list[i]) > 0 : 
+    buildYaraStructure(list[0], i)
 
 echo cpuTime() - init
