d36f33abfd5be6428bce0d73e74f6c1805e94569
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-01-27 23:36:55 +0200

    Update and rename HideNSeek.nim to info.nim

---
+++ src/info.nim
@@ -0,0 +1,16 @@
+echo """
+    Usage: hidenseek [OPTION]... [FILE] / [PATH]
+    File manager and malware detector.
+
+    With no FILE / PATH, or when FILE / PATH is -, read standard input.
+
+    -A, --show-all           [PATh] Shows if a file from a path have 
+                                an extension changed by checking its hex 
+                                decimal header.
+
+    Examples:
+    nim c -r hidenSeek.nim -A /home/{username}/Desktop
+    #The output is only when a suspicious file is found.
+
+    Full documentation <https://github.com/Zaque-69/Hide-N-Seek>
+    """
