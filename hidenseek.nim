f66e6d5c045542f6822b236e897cc0f6c35c9167
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-01-30 11:04:09 +0200

    Update hidenseek.nim

--- hidenseek.nim
+++ hidenseek.nim
@@ -1,11 +1,19 @@
-import osproc, strformat ,std/os
+import osproc, strformat
+import std/[os]
+
+var
+    local : seq[string] = @["ransomware", "miner"]
 
 proc runCommad( command : string ) : void =
     let run : int = execCmd(command)
 
 case paramStr(1):
-    of "-h" : 
-        runCommad("cd src && nim c -r info.nim && cd ..")
-    of "-i" : 
-        runCommad(fmt"cd src && nim c -r buildYara.nim {paramStr(2)} && cd ..")
-    else : discard
+    of "-a" : 
+        runCommad(fmt"clear && cd src && nim c -r buildYara.nim {paramStr(2)} && cd ..")
+    of "-m" : 
+        for i in countup(0, len(local) - 1) : 
+            runCommad(fmt"clear && yara src/malware/{local[i]}.yara {paramStr(2)}")
+    of "-s" :  
+        runCommad(fmt"python src/python/sortFilesComp.pyc {paramStr(2)}")
+    else : 
+        runCommad("clear && cd src && nim c -r info.sh && cd ..")
