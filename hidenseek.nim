b93533275ba8ee3cacfe0f38a71c37b4c86ba5fe
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-01-31 23:38:13 +0200

    Update hidenseek.nim

--- hidenseek.nim
+++ hidenseek.nim
@@ -4,16 +4,18 @@ import std/[os]
 var
     local : seq[string] = @["ransomware", "miner"]
 
-proc runCommad( command : string ) : void =
+proc runCommand( command : string ) : void =
     let run : int = execCmd(command)
 
 case paramStr(1):
-    of "-a" : 
-        runCommad(fmt"clear && cd src && nim c -r buildYara.nim {paramStr(2)} && cd ..")
-    of "-m" : 
-        for i in countup(0, len(local) - 1) : 
-            runCommad(fmt"clear && yara src/malware/{local[i]}.yara {paramStr(2)}")
-    of "-s" :  
-        runCommad(fmt"python src/python/sortFilesComp.pyc {paramStr(2)}")
-    else : 
-        runCommad("clear && cd src && nim c -r info.sh && cd ..")
+    of "-a" :
+        runCommand(fmt"cd src && nim c -r buildYara.nim {paramStr(2)} && cd ..")
+    of "-m" :
+        runCommand("clear")
+        for i in countup(0, len(local) - 1) :
+            #echo fmt"yara src/malware/{local[i]}.yara {paramStr(2)}"
+            runCommand(fmt"yara src/malware/{local[i]}.yara {paramStr(2)}")
+    of "-s" :
+        runCommand(fmt"python3 src/python/sortFilesComp.pyc {paramStr(2)}")
+    else :
+        runCommand("clear && cd src && nim c -r info.sh && cd ..")
