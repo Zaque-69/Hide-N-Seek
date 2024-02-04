b84b048e13b2820abb69df6474d7a6478ca65247
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-02-04 21:32:59 +0200

    Update hidenseek.nim

--- hidenseek.nim
+++ hidenseek.nim
@@ -1,21 +1,22 @@
-import osproc, strformat
+import osproc, strformat, times
 import std/[os]
 
-var
-    local : seq[string] = @["ransomware", "miner"]
+from src/runCommand import runShellCommand
 
-proc runCommand( command : string ) : void =
-    let run : int = execCmd(command)
+let t : float = cpuTime()
+
+runShellCommand("clear")
 
 case paramStr(1):
-    of "-a" :
-        runCommand(fmt"cd src && nim c -r buildYara.nim {paramStr(2)} && cd ..")
-    of "-m" :
-        runCommand("clear")
-        for i in countup(0, len(local) - 1) :
-            #echo fmt"yara src/malware/{local[i]}.yara {paramStr(2)}"
-            runCommand(fmt"yara src/malware/{local[i]}.yara {paramStr(2)}")
-    of "-s" :
-        runCommand(fmt"python3 src/python/sortFilesComp.pyc {paramStr(2)}")
-    else :
-        runCommand("clear && cd src && nim c -r info.sh && cd ..")
+    of "-a" : #all
+        runShellCommand(fmt"cd src && ./buildYara {paramStr(2)} && cd ..")
+    of "-m" : #malware
+        echo "pass"
+    of "-s" :  #sort
+        runShellCommand(fmt"python3 src/python/sortFilesComp.pyc {paramStr(2)}")
+    of "-p" :  #process
+        runShellCommand("")
+    else : 
+        runShellCommand("clear && source info.sh && help")
+
+echo "Execution time : ", cpuTime() - t
