56a5a6c8dcc669b0a012a62dd5ca3bd9d0c50512
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-02-20 23:08:20 +0200

    Update runCommand.nim

--- src/runCommand.nim
+++ src/runCommand.nim
@@ -1,4 +1,11 @@
 import osproc, strutils
+import std/[terminal, strformat]
 
-proc runShellCommand*(command: string): void = 
+proc runShellCommand*(command: string) : void = 
   let result = execCmd(command)
+
+proc echoStatusFile*(file : string, boolean : bool) : void = 
+  if boolean : 
+    stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {file} has passed the test!")
+  else : 
+    stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} has extension changed!") 
