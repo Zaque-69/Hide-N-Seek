bcb876e6c1414165e717583b78f7397638eb07be
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-02-20 23:07:22 +0200

    Update process.nim

--- src/process.nim
+++ src/process.nim
@@ -17,24 +17,21 @@ proc deleteLineFromString(inputStr: string, lineNum: int): string =
     
     return lines.join("\n")
 
-#extraxcting all the processes name files from after running the C compiled file
-#runShellCommand("touch c/process.txt && c/process && mv c/process.txt .")
+runShellCommand("clear && touch c/process.txt && c/process && mv c/process.txt . && sudo su")
+for i in yaraRules:
+  for line in lines "process.txt" : 
+    try :
 
-if paramStr(1) == "-p" : 
-  runShellCommand("clear && touch c/process.txt && c/process && mv c/process.txt . && sudo su")
-  for i in yaraRules:
-    for line in lines "process.txt" : 
-      try :
-        if contains(readFile("aux.txt"), "error") :
-          discard
-
-        elif len(readFile("aux.txt")) == 0 :
-          stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {line} has passed the test!", readFile("yaraLines.txt"))
-
-        else : 
-          stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {line} may be malitious! Reason : ", deleteLineFromString(readFile("yaraLines.txt"), 1))
-        
-      except:
+      if contains(readFile("aux.txt"), "error") :
         discard
+
+      elif len(readFile("aux.txt")) == 0 :
+        stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {line} has passed the test!", readFile("yaraLines.txt"))
+      else :
+
+        stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {line} may be malitious! Reason : ", deleteLineFromString(readFile("yaraLines.txt"), 1))
+        
+    except:
+      discard
 
 echo "\nExecution time : ", cpuTime() - t
