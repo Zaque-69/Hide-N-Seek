57246a42d96f9dbf4d69faf056a0b20d23ac90cd
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-03-28 11:50:41 +0200

    Update runCommand.nim

--- src/runCommand.nim
+++ src/runCommand.nim
@@ -2,10 +2,14 @@ import osproc
 import std/[terminal, strformat]
 
 proc runShellCommand*(command: string) : void = 
-  let result = execCmd(command)
+  discard execCmd(command)
 
 proc echoStatusFile*(file, rule : string, boolean : bool) : void = 
   if boolean : 
     stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {file} has passed the {rule} test!")
-  else : 
-    stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} may be malitious. Reason : {rule}.") 
+  else : stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} may be malitious. Reason : {rule}.") 
+
+proc has_extension_changed*( file : string, boolean : bool ) : void = 
+  if not boolean : 
+    stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {file} has passed the test!")
+  else : stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} has extension changed!") 
