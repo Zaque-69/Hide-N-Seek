d8fb39df6a23d2959b3853ef24a2838bf5dc6b09
Z4que <114612941+Zaque-69@users.noreply.github.com>
2024-05-27 23:44:52 +0300

    Update runCommand.nim

--- src/runCommand.nim
+++ src/runCommand.nim
@@ -1,15 +1,23 @@
 import osproc
-import std/[terminal, strformat]
+import std/[ terminal, strformat, os ]
+import std/strutils
 
-proc runShellCommand*(command: string) : void = 
+proc runShellCommand * ( command: string,  ) : void = 
   discard execCmd(command)
 
-proc echoStatusFile*(file, rule : string, boolean : bool) : void = 
-  if boolean : 
-    stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {file} has passed the {rule} test!")
-  else : stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} may be malitious. Reason : {rule}.") 
+proc compile_all * ( path, rm_ext : string ) : void =
+  for file in walkDir( fmt "{path}") :
+    if contains( file.path, "nim" ) : 
+      runShellCommand( fmt "nim c {file.path}" )
+    else : 
+      runShellCommand( fmt "yarac {file.path} {file.path[0 .. len( file.path) - 5 ] }" )
+    
+  for file in walkDir( fmt "{path}") :
+    if contains( file.path, rm_ext ) : 
+      runShellCommand( fmt "rm {file.path}" )
 
-proc has_extension_changed*( file : string, boolean : bool ) : void = 
-  if not boolean : 
-    stdout.styledWriteLine(fgGreen, styleBright, fmt"[0K!] File : {file} has passed the test!")
-  else : stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} has extension changed!") 
+proc echoStatusFile * ( file, rule : string, boolean : bool ) : void = 
+  if boolean : stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} may be malitious. Reason : {rule}.") 
+
+proc has_extension_changed * ( file : string, boolean : bool ) : void = 
+  if boolean : stdout.styledWriteLine(fgRed, styleBright, fmt"[WARNING!] File : {file} has extension changed!") 
